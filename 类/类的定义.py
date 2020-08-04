7f87e0d97fd5dbbb9905f0cb421501459a548bdd
peter_young <769130635@qq.co,qq.co>
2020-08-04 18:42:41 +0800

    8.4

---
+++ 类/类的定义.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Time : 8/4/2020 2:28 PM
+# @Author : Peter yang
+
+
+def run():
+    pass
+
+
+class Money:
+    pass
+
+
+## 打印类名
+print(Money.__name__)
+xxx = Money
+print(xxx.__name__)
+print(Money)
+## 产生对象
+one = Money()
+print(one.__class__)
